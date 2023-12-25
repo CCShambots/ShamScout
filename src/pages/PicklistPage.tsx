ea1f77e2163d465f3e8188cf86031efc91b59c22
Jacob Barta <jacob.barta25@gmail.com>
2023-12-25 15:27:53 -0600

    add import and export function

--- src/pages/PicklistPage.tsx
+++ src/pages/PicklistPage.tsx
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {createRef, useEffect, useRef, useState} from "react";
 import AppHeader from "../components/header/AppHeader";
 import {ReactSortable} from "react-sortablejs";
 import "./PicklistPage.css"
 import {useLocalStorage} from "usehooks-ts";
 import TeamLink from "../components/team-link/TeamLink";
-import {Button, Checkbox, Icon} from "semantic-ui-react";
+import {Button, Checkbox, Dimmer, Header, Icon, Input} from "semantic-ui-react";
 import {ACCEPT_LIST, CURRENT_EVENT, DECLINE_LIST, PICKLIST, TEAMS} from "../util/LocalStorageConstants";
 import StatsPopoutManager from "../components/picklist/StatsPopoutManager";
+import {CSVLink} from "react-csv";
+import {upload} from "@testing-library/user-event/dist/upload";
+
 
 type Team = {
     name:string,
@@ -37,10 +40,16 @@ function PicklistPage() {
     const [privacyDimmer, setPrivacyDimmer] = useState(false)
 
     const [teamStatsList, setTeamStatsList] = useState<{number: number, name:string}[]>([])
-    
+
+    let downloadCSVRef:any = createRef()
+
+    let uploadCSVRef:any = createRef()
+
+    let [downloadData, setDownloadData] = useState<string[][]>([])
+    let [importConfirmationActive, setImportConfirmationActive] = useState(false)
+
     //Load info
     useEffect(() => {
-
         let picksToApply = savedPicks;
 
         //Load picks into the saved picks if there are none present
@@ -84,9 +93,99 @@ function PicklistPage() {
         }
     }
 
+    useEffect(() => {
+
+        let newData:string[][] = []
+
+        let maxLen = Math.max(pickList.length, acceptList.length, declineList.length)
+
+        for(let i = 0; i<maxLen; i++) {
+            let row:string[] = []
+            if(pickList[i]) {
+                row.push(pickList[i].team.number.toString() + "-" + pickList[i].team.name)
+            } else {
+                row.push("")
+            }
+
+            if(acceptList[i]) {
+                row.push(acceptList[i].team.number.toString()+ "-" + acceptList[i].team.name)
+            } else {
+                row.push("")
+            }
+
+            if(declineList[i]) {
+                row.push(declineList[i].team.number.toString()+ "-" + declineList[i].team.name)
+            } else {
+                row.push("")
+            }
+
+            newData.push(row)
+        }
+
+        setDownloadData([...newData])
+
+    }, [pickList, acceptList, declineList]);
+
+    const fileReader = new FileReader();
+
+
+    const handleUploadChange = (e:any) => {
+
+        if (e.target.files[0]) {
+            fileReader.onload = function (event) {
+                const csvOutput = event.target?.result;
+
+                let splitRows = (csvOutput! as string).replaceAll('"', '').split("\n")
+
+                let splitEntries = splitRows.map(e => e.split(","))
+
+                console.log(splitEntries)
+
+                let pick:Team[] = []
+                let accept:Team[] = []
+                let decline:Team[] = []
+
+                for(let i = 1; i<splitEntries.length; i++) {
+                    for(let col = 0; col<3; col++) {
+                        if(splitEntries[i][col] === "") continue
+
+                        let thisVal = splitEntries[i][col].split("-")
+                        console.log(thisVal)
+                        if(col === 0) {
+                            pick.push({number: parseInt(thisVal[0]), name: thisVal[1]})
+                        } else if(col === 1) {
+                            accept.push({number: parseInt(thisVal[0]), name: thisVal[1]})
+                        } else {
+                            decline.push({number: parseInt(thisVal[0]), name: thisVal[1]})
+                        }
+
+                    }
+                }
+
+                setSavedPicks(pick)
+                setSavedAccepts(accept)
+                setSavedDeclines(decline)
+
+                window.location.reload()
+            };
+
+            fileReader.readAsText(e.target.files[0]);
+
+        }
+    };
+
     return (
         <div>
             <AppHeader/>
+            <Button.Group fluid>
+                <Button color={"blue"} onClick={() => downloadCSVRef.current.link.click()}><Icon name={"download"}/>Export</Button>
+                <Button color={"green"} onClick={() => setImportConfirmationActive(true)}><Icon name={"upload"}/>Import</Button>
+                <Button color={"red"}><Icon name={"redo"}/>Reset to Event Rankings</Button>
+            </Button.Group>
+
+            <CSVLink ref={downloadCSVRef} headers={["pick", "accept", "decline"]} data={downloadData} filename={`picklist-${currentEvent}.csv`}/>
+
+            <input ref={uploadCSVRef} className={"hide-csv-upload"} type={"file"} accept={".csv"} onChange={handleUploadChange}/>
 
             <div className={"picklist-flex"}>
                 <div className={"picklist-column"}>
@@ -179,6 +278,16 @@ function PicklistPage() {
                 className={(privacyDimmer) ? "privacy-active" : "privacy-inactive"}
                 onClick={() => setPrivacyDimmer(false)}
             />
+
+            <Dimmer page active={importConfirmationActive} onClickOutside={() => {setImportConfirmationActive(false)}}>
+                <Header inverted>
+                    This will overwrite your current picklist. Are you sure you want to continue?
+                </Header>
+                <Button color={"red"} onClick={() => {
+                    uploadCSVRef.current.click()
+                    setImportConfirmationActive(false)
+                }}>Yes, I'm Sure</Button>
+            </Dimmer>
 
         </div>
     )
